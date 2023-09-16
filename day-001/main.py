1b6db061c209bd078f56a17c02406af279eec96d
Rafael Santos <e.rafael.rss@gmail.com>
2023-09-15 23:51:26 -0300

    feat: Complete Day 1 - Introduction to Python
    - Aula 1 do curso '100 Days of Code: The Complete Python Pro Bootcamp'
    - Imprimindo no console em Python
    - Manipulação de Strings e Inteligência de Código
    - A função de entrada Python (input)
    - Variáveis Python e suas nomenclaturas
    - Projeto do Dia 1: Gerador de Nome de Banda
    
    Closes #1

---
+++ day-001/main.py
@@ -0,0 +1,73 @@
+##### LESSON 1 ######
+# print("Ola Mundo")
+
+# print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what  print')")
+# #print("The function is declared like this:")
+# #print("print('what  print')")
+# print ("Hello" + " " + "Angela")
+
+##### LESSON 2 ######
+# Fix the code below 👇
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+##### LESSON 3 ######
+
+# print("Hello " + input("Whats your name") + "!")
+
+##### LESSON 4 ######
+
+# print(
+#     len(
+#     input("Escreva seu nome aqui !")
+#     )
+# )
+
+# your_name = input("Escreva seu nome aqui ! \n")
+# tamanho = len (your_name)
+# print(tamanho)
+
+# ##### LESSON 5 ######
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# c = a
+# a = b
+# b = c
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+# 1. Create a greeting for your program.
+
+print("Welcome to the band name creation software")
+
+# 2. Ask the user for the city that they grew up in.
+
+your_city = input(" Where are you from ? \n")
+
+# 3. Ask the user for the name of a pet.
+
+your_pet = input("Whats name your pet ?")
+
+# 4. Combine the name of their city and pet and show them their band name.
+
+band_name = (your_city + " " + your_pet)
+
+# 5. Make sure the input cursor shows on a new line, see the example at:
+#    https://band-name-generator-end.appbrewery.repl.run/
+
+print("O nome da sua banda e " + band_name + "!")
