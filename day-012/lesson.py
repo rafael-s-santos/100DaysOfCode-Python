5254a7d56532e4d2cf7ffaab781f23f06da6b878
Rafael Santos <e.rafael.rss@gmail.com>
2023-10-02 23:47:57 -0300

    feat: Complete Day 12 - Beginner - Scope & Number Guessing Game
    - Aula 12 do curso '100 Days of Code: The Complete Python Pro Bootcamp'
    - Python tem escopo de bloco?
    - Como modificar uma variável global
    - Constantes Python e escopo global
    - Apresentando o projeto final: o jogo de adivinhação de números

---
+++ day-012/lesson.py
@@ -0,0 +1,77 @@
+# ################### Scope ####################
+
+# enemies = 1
+
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# # Local Scope
+
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+
+# drink_potion()
+# # print(potion_strength)
+
+# # Global Scope
+# player_helth = 10
+
+# def game():
+
+#     def drink_potion():
+#         potion_strength = 2
+#         print(player_helth)
+
+#     drink_potion()
+
+# print(player_helth)
+
+# There is no BLock Scope
+
+# game_level = 3
+# enemies = ["Skeleton", "Zombie", "Alien"]
+# if game_level < 5:
+#     new_enemy = enemies[0]
+
+# print(new_enemy)
+
+# # Modifying Global Scope
+
+# enemies = 1
+
+# def increase_enemies():
+#     global enemies
+#     enemies += 1
+#     print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+enemies = 1
+
+
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Global Constants
+
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@rafa_qwerty_"
+
+
+def calc():
+    return PI
+
+
+print(calc())
