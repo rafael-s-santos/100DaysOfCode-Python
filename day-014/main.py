57ade147b6c0aec549ab21f98823a9019c955a9e
Rafael Santos <e.rafael.rss@gmail.com>
2023-10-04 22:12:44 -0300

    WIP: Day 14 - Beginner - Higher Lower Game Project

---
+++ day-014/main.py
@@ -0,0 +1,27 @@
+from art import logo, vs
+from game_data import data
+
+# CODE OUTLINE:
+
+# end_game = False
+# score_value = 0
+# While end_game = False
+# random list data A (0, len(data) -1)
+# random list data B, A != B
+# logo
+# If score_value == 0, not show
+# else, You're right! Current score: score_value.
+# Compare A: {name}, a {description}, from {country}.
+# vs
+# Compare B: {name}, a {description}, from {country}.
+# Who has more followers? Type 'A' or 'B':
+# If follower_count A > follower_count B
+# answer = A
+# else, answer = B
+# If guess == answer
+# return score_value += 1
+# clear
+# else:
+# clear
+# logo
+# Sorry, that's wrong. Final score: {score_value} - end_game = True
