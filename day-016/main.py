e6292a964e0ec0b1f56226eaea076fb61ffad2c7
Rafael Santos <e.rafael.rss@gmail.com>
2023-10-08 23:09:11 -0300

    WIP: Day 16 - Intermediate - Object Oriented Programming(OOP)

---
+++ day-016/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+
+my_screen = Screen()
+my_screen.canvheight
+print(my_screen.canvheight)
+my_screen.exitonclick()
