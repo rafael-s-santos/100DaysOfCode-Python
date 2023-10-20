f0a91af308a7a896e18f7d4bb8be77657ee07f53
Rafael Santos <e.rafael.rss@gmail.com>
2023-10-19 23:58:27 -0300

    feat: Complete Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)
    - Aula 18 do curso '100 Days of Code: The Complete Python Pro Bootcamp'
    - Compreendendo os Turtle Graphics e como usar a documentação
    - Desafio da Tartaruga 1 - Desenhe um Quadrado
    - Importando Módulos, Instalando Pacotes e Trabalhando com Aliases
    - Turtle Challenge 2 - Desenhe uma linha tracejada
    - Desafio da Tartaruga 3 - Desenhando Formas Diferentes
    - Desafio da Tartaruga 4 - Gere um Passeio Aleatório
    - Tuplas Python e como gerar cores RGB aleatórias
    - Desafio da Tartaruga 5 - Desenhe um Espirógrafo
    - The Hirst Painting Project Parte 1 - Como extrair valores RGB de imagens
    - O Projeto de Pintura Hirst Parte 2 - Desenhando os Pontos

---
+++ day-018/#03_importing_modules_-_installing_packages_and_working_aliases.py
@@ -0,0 +1,25 @@
+# keyword / Module name
+# import turtle
+
+# tim = turtle.Turtle()
+
+# #-------------
+# # keyword / Module name /keyword / Thing in module
+# from turtle import Turtle
+
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+
+# # keyword / Module name /keyword / Everything
+# from turtle import *
+
+# forward(90)
+
+
+### ALIASES ###
+
+# # Keyword / Module name /keyword /Alias name
+import turtle as t
+
+tim = t.Turtle()
