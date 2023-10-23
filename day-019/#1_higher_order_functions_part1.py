57b89095d27af0c6e5a4164fbec66f04c8c93018
Rafael Santos <e.rafael.rss@gmail.com>
2023-10-23 16:47:24 -0300

    feat: Complete Day 19 - Intermediate - Instances, State and Higher Order Functions
    - Aula 19 do curso '100 Days of Code: The Complete Python Pro Bootcamp'
    - Funções de ordem superior
    - Desafio: Faça um aplicativo Etch-A-Sketch
    - Estado do objeto e instâncias
    - Compreendendo o sistema de coordenadas do turtle
    - Eee, vamos para as corridas!

---
+++ day-019/#1_higher_order_functions_part1.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+# Tim, the turtle
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+
+
+screen.exitonclick()
---
+++ day-019/#3_challenge_make_an_etch-a-sketch_app.py
@@ -0,0 +1,37 @@
+# W = FOrwards
+# S Backwards
+# A = Counter-Clockwise
+# D = Clockwise
+# C = Clear drawing
+
+from turtle import Turtle, Screen, resetscreen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+screen.listen()
+screen.onkey(move_forwards, 'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(turn_left, 'a')
+screen.onkey(turn_right, 'd')
+screen.onkey(resetscreen, 'c')
+screen.exitonclick()
