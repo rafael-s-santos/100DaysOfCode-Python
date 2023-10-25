7cdb971723a1735e59af92535bcd53ffad580d95
Rafael Santos <e.rafael.rss@gmail.com>
2023-10-25 11:21:10 -0300

    feat: Complete Day 20 - Intermediate - Build the Snake Game Part 1: Animation & Coordinates
    - Aula 20 do curso '100 Days of Code: The Complete Python Pro Bootcamp'
    - Configuração de tela e criação de um Snake Body
    - Animando os segmentos de cobra na tela
    - Crie uma classe Snake e mudando para OOP
    - Como controlar a cobra pressionando uma tecla

--- day-020/main.py
+++ day-020/main.py
@@ -1,27 +1,27 @@
-from turtle import Screen, Turtle
-
+from turtle import Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('Snake Game')
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-for position in starting_positions:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.goto(position)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-segment_1 = Turtle('square')
-segment_1.color('white')
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-segment_2 = Turtle('square')
-segment_2.color('white')
-segment_2.goto(-20, 0)
+    snake.move()
 
-segment_3 = Turtle('square')
-segment_3.color('white')
-segment_3.goto(-40, 0)
 
 screen.exitonclick()
