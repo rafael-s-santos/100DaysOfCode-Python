404400ddf7953548ae97b18f72a7d3e1a68ae285
Rafael Santos <e.rafael.rss@gmail.com>
2024-01-04 02:26:25 -0300

    WIP: Day 35 - Keys, Authetication & Environment Variables: Send SMS

--- day-035/main.py
+++ day-035/main.py
@@ -6,9 +6,22 @@ api_key = "fb423834d3fc46f38754a0ce8d452535"
 weather_params = {
     "lat": -20.326280,
     "lon": -40.391820,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
 response = requests.get(OWN_Endpoint, params=weather_params)
 
-print(response.json())
+response.raise_for_status()
+
+weather_data = response.json()
+
+will_rain = False
+for num_list in weather_data["list"]:
+    condition_code = num_list["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+        break
+    
+if will_rain:
+    print("Bring an umbrella.")
